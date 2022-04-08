faa664208388c59e97ebc97c1b6feec05828ca0f
hafeezulkareem <hafeezulkareem20@gmail.com>
2022-04-08 14:52:46 +0530

    Complete star-rating challenge

--- star-rating/script.js
+++ star-rating/script.js
@@ -5,5 +5,52 @@
  * @param callback Returns selected star count to callback
  */
 function Star(el, count, callback) {
-    
+   let rating = 0;
+
+   const starsContainer = document.querySelector(el);
+   for (let i = 0; i < count; i++) {
+      const star = document.createElement("i");
+      star.classList = "fa fa-star-o";
+      star.addEventListener("mouseover", () => onHoverStar(i));
+      star.addEventListener("click", () => {
+         rating = i + 1;
+         callback(rating);
+      });
+      starsContainer.appendChild(star);
+   }
+   starsContainer.addEventListener("mouseleave", () => clearRating(rating));
+}
+
+function onHoverStar(index) {
+   const stars = document.querySelectorAll(".fa");
+   stars.forEach((star) => {
+      if (star.classList.contains("fa-star")) {
+         star.classList.remove("fa-star");
+      }
+      star.classList.add("fa-star-o");
+   });
+
+   for (let i = 0; i <= index; i++) {
+      stars[i].classList.remove("fa-star-o");
+      stars[i].classList.add("fa-star");
+   }
+}
+
+function clearRating(rating) {
+   const stars = document.querySelectorAll(".fa");
+   let i = 0;
+   while (i < rating) {
+      if (stars[i].classList.contains("fa-star-o")) {
+         stars[i].classList.remove("fa-star-o");
+      }
+      stars[i].classList.add("fa-star");
+      i += 1;
+   }
+   while (i < stars.length) {
+      if (stars[i].classList.contains("fa-star")) {
+         stars[i].classList.remove("fa-star");
+      }
+      stars[i].classList.add("fa-star-o");
+      i += 1;
+   }
 }
